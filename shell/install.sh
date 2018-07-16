ec1c7352781b0dedf59ea27d7c9981ad45617cdd
shigaki daichi <spike-19@ezweb.ne.jp>
2018-07-16 16:17:59 +0900

    Update install.sh

--- shell/install.sh
+++ shell/install.sh
@@ -11,6 +11,9 @@ for list in ${packagelist[@]}; do
 	sudo apt-get install -y ${list}
 done
 
+# install manager on vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
